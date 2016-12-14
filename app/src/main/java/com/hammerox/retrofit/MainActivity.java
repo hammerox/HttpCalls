905ed64ccabd5127b1a9d29015c11a08f41a16c8
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-13 23:00:48 -0200

    Prepared for Retrofit

--- app/src/main/java/com/hammerox/retrofit/MainActivity.java
+++ app/src/main/java/com/hammerox/retrofit/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mLoginView;
     EditText mPasswordView;
+    TextView mAsyncTaskTextView;
+    TextView mOkHttpTextView;
+    TextView mRetrofitTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         mLoginView = (EditText) findViewById(R.id.login);
         mPasswordView = (EditText) findViewById(R.id.password);
+        mAsyncTaskTextView = (TextView) findViewById(R.id.text_asynctask);
+        mOkHttpTextView = (TextView) findViewById(R.id.text_okhttp);
+        mRetrofitTextView = (TextView) findViewById(R.id.text_retrofit);
     }
 
 
